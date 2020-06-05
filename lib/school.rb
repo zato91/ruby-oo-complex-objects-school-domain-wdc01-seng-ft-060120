0e2505a12f3cfd87a87c92d7e8afcee9fedab027
zara ahmadi <zatokey@gmail.com>
2020-06-05 05:04:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,13 @@
 # code here!
 require "pry"
 class School
+attr_accessor :roster
 
   def initialize(name)
     @name = name 
     @roster = {}
   end
-  def roster
-    @roster
-  end
+
   
  def add_student(name, grade)
  
