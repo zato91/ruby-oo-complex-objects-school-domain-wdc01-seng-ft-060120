c0568cbfe1b6034ded98d431fbf886bdd2950ccd
zara ahmadi <zatokey@gmail.com>
2020-06-05 05:18:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,9 +10,10 @@ attr_accessor :roster
 
   
  def add_student(name, grade)
-   binding.pry
- #if  @roster[grade] 
-   @roster[grade]  = []
+ 
+ if  @roster[grade].nil? 
+    @roster[grade]  = []
+  end 
     @roster[grade]  << name 
   
  end 
