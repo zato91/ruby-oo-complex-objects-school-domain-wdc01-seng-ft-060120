5a6ba32c38aecf86532e34eefd8411edd100f3a7
zara ahmadi <zatokey@gmail.com>
2020-06-05 05:00:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require "pry"
+class School
+
+  def initialize(name)
+    @name = name 
+  end
+  
+  
+  def roster
+    roster_hash = {}
+  end 
+  binding.pry
+ def add_student(name, grade)
+   self.roster[:grade] = []
+   self.roster[:grade]  << name 
+  
+ end 
+ 
+ def grade(grade)
+ end 
+ 
+ def sort
+ end 
+ 
+end 
